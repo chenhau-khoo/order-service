ab29c7c7eab72caad4d51cb1cab5da56c4cba4a3
Chen Hau <ckhoo@moneylion.com>
2020-10-17 08:35:52 +0800

    add payments module add payments service add payments controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersModule } from './modules/orders/orders.module';
+import { PaymentsModule } from './modules/payments/payments.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), OrdersModule],
+  imports: [TypeOrmModule.forRoot(), OrdersModule, PaymentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
