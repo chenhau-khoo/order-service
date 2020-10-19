a617a8f3305fa67a974e79b2dd33da882db50434
Chen Hau <ckhoo@moneylion.com>
2020-10-19 17:57:23 +0800

    update docker-compose to join same network

--- src/modules/payments/payments.module.ts
+++ src/modules/payments/payments.module.ts
@@ -1,10 +1,10 @@
 import { HttpModule, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PaymentsService } from './payments.service';
 
 @Module({
-  imports: [HttpModule.register({
-    timeout: 5000
-  })],
+  imports: [HttpModule.register({ timeout: 5000 }),
+    ConfigModule],
   providers: [PaymentsService],
   controllers: [],
   exports: [PaymentsService]
--- src/modules/payments/payments.service.ts
+++ src/modules/payments/payments.service.ts
@@ -1,4 +1,5 @@
 import { HttpService, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { MakePaymentReq } from './dto/make-payment.req';
@@ -6,10 +7,11 @@ import { MakePaymentResp } from './dto/make-payment.resp';
 
 @Injectable()
 export class PaymentsService {
-    constructor(private readonly httpService: HttpService) { }
+    constructor(private readonly httpService: HttpService,
+        private configService: ConfigService) { }
 
     makePayment(req: MakePaymentReq): Observable<MakePaymentResp> {
-        return this.httpService.post('http://localhost:3001/payment', req)
+        return this.httpService.post(this.configService.get('PAYMENT_API_URL') + '/payment', req)
             .pipe(
                 map(resp => resp.data)
             );
